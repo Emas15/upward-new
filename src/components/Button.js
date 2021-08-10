eb8470712fdae4937a6db018b51fbe1e7bd64aac
Ashphalt <asfaqueeshan721@gmail.com>
2021-08-10 13:44:32 +0600

    css directory changed

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Button.css';
+import '../assets/css/Button.css';
 import { Link } from 'react-router-dom';
 
 export function Button() {
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Button } from './Button';
 import { Link } from 'react-router-dom';
-import './Navbar.css';
+import '../assets/css/Navbar.css';
 import Dropdown from './Dropdown';
 import DropdownTeam from './DropdownTeam';
 import {NavbarBrand} from 'reactstrap'
